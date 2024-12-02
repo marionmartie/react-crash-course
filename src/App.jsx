0c9cc5e07905a58754991446c27ec9846869ba7e
Marion Balba <balba.marion@gmail.com>
2024-12-02 13:13:54 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className='text-5xl'>App</div>
+    <div className='text-5xl'>App</div>
+    </>
+  )
+}
+
+export default App
