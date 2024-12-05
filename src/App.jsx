7b75b932c0b0c7f935a5c79edb4c2549a978ab4f
Marion Balba <balba.marion@gmail.com>
2024-12-05 08:24:47 +0800

    Day 9 - routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import React from 'react'
-import NavBar from './components/NavBar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
-
+import HomePage from './pages/HomePage'
+import MainLayout from './layouts/MainLayout'
+import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 const App = () => {
-  return (
-    <>
-    <NavBar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
-
-    <ViewAllJobs />
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='/' element={ <MainLayout /> } >
+      <Route index element={ <HomePage /> } />
+      <Route path='/jobs' element={ <JobsPage /> } />
+      <Route path='*' element={ <NotFoundPage /> } />
+    </Route>
+    )
   )
+  return <RouterProvider router={router} />
 }
 
 export default App
